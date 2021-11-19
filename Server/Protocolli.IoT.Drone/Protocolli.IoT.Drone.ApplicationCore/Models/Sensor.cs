ae3ad78d8fe3893f3955fafcc9b17c022cee6ae4
Matteo Faraoni <matteo.faraoni@stud.tecnicosuperiorekennedy.it>
2021-11-19 17:07:16 +0100

    aggiunta delle classi

---
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ApplicationCore/Models/Sensor.cs
@@ -0,0 +1,7 @@
+﻿namespace Protocolli.IoT.Drone.ServerApp.Models
+{
+	public class Sensor
+	{
+		public double Timestamp { get; set; }
+	}
+}
---
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ServerApp/Controllers/DroneController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Protocolli.IoT.Drone.ServerApp.Controllers
+{
+	public class DroneController
+	{
+		[Route("api/[controller]")]
+		[ApiController]
+		public class ProductController : ControllerBase
+		{
+			private readonly IProductService _productService;
+
+			public ProductController(IProductService productService)
+			{
+				_productService = productService;
+			}
+
+		}
+}
