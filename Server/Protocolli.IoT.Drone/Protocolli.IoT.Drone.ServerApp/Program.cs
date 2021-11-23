d0e5a3f78033948e39d78e666279e5f3f8a57d3d
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-11-23 17:01:05 +0100

    Minor fix

--- Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ServerApp/Program.cs
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ServerApp/Program.cs
@@ -15,7 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 // API Versioning
 builder.Services.AddApiVersioning();
