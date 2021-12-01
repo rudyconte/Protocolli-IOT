fa982e612c125cd42895c2811a9abfa6bae440ea
Kevin Mainardis <kevin.mainardis@stud.tecnicosuperiorekennedy.it>
2021-12-01 14:10:59 +0100

    New methods

--- Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ApplicationCore/Interfaces/Data/IDroneStatusRepository.cs
+++ Server/Protocolli.IoT.Drone/Protocolli.IoT.Drone.ApplicationCore/Interfaces/Data/IDroneStatusRepository.cs
@@ -4,6 +4,9 @@ namespace Protocolli.IoT.Drone.ApplicationCore.Interfaces.Data
 {
     public interface IDroneStatusRepository
     {
-        void Insert(DroneStatus value);
+        void Insert(DroneStatus status);
+        DroneStatus GetLastById(int droneId);
+        IEnumerable<DroneStatus> GetAllById(int droneId);
+        IEnumerable<DroneStatus> GetAll();
     }
 }
