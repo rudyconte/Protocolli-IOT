c79487db155ea69498903d9e8bb78bf3e0164b93
Mattia Ros <mattia.ros@stud.tecnicosuperiorekennedy.it>
2021-12-02 17:48:16 +0100

    Added MQTT Subscriber

---
+++ Server/Protocolli.IoT.Drone/Protocolli.Iot.Drone.Subscriber/Mqtt.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using Protocolli.IoT.Drone.ApplicationCore.Interfaces.Services;
+using Protocolli.IoT.Drone.ApplicationCore.Models;
+
+namespace Protocolli.Iot.Drone.Subscriber
+{
+    public class Mqtt
+    {
+        private readonly IDroneStatusService _droneStatusService;
+        private readonly IMqttClient _mqttClient;
+        public Mqtt(IDroneStatusService droneStatusService)
+        {
+            _droneStatusService = droneStatusService;
+            // Create a new MQTT client.
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+
+            // Use TCP connection.
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer("127.0.0.1", 1883) // Port is optional
+                .Build();
+
+            _mqttClient.ConnectAsync(options, CancellationToken.None);
+            _mqttClient.UseConnectedHandler(async e =>
+            {
+                Console.WriteLine("### CONNECTED WITH SERVER ###");
+
+                // Subscribe to a topic
+                await _mqttClient.SubscribeAsync(new MqttClientSubscribeOptionsBuilder().WithTopicFilter("protocolliIOT/stato/#").Build());
+
+                Console.WriteLine("### SUBSCRIBED ###");
+            });
+            _mqttClient.UseApplicationMessageReceivedHandler(e =>
+            {
+                Console.WriteLine("### RECEIVED APPLICATION MESSAGE ###");
+                string Payload = Encoding.UTF8.GetString(e.ApplicationMessage.Payload);
+                DroneStatus status = JsonSerializer.Deserialize<DroneStatus>(Payload);
+            });
+
+
+        }
+
+
+    }
+}
---
+++ Server/Protocolli.IoT.Drone/Protocolli.Iot.Drone.Subscriber/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Protocolli.Iot.Drone.Subscriber;
+using Protocolli.IoT.Drone.ApplicationCore.Interfaces.Data;
+using Protocolli.IoT.Drone.ApplicationCore.Interfaces.Services;
+using Protocolli.IoT.Drone.ApplicationCore.Services;
+using Protocolli.IoT.Drone.Infrastructure.Data;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+                       .ConfigureServices((_, services) =>
+               services.AddScoped<IDroneStatusService, DroneStatusService>()
+                       .AddScoped<IDroneStatusRepository, DroneStatusRepository>())
+                       .Build();
+
+Mqtt mqtt = new Mqtt(); 
+
+Console.ReadLine(); 
+
