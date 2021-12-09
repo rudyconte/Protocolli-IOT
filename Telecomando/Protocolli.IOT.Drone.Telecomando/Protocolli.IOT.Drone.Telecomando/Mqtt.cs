37f0154aaeb4c88e705612b7566c20e794b1b06d
Mattia Ros <mattia.ros@stud.tecnicosuperiorekennedy.it>
2021-12-09 10:28:55 +0100

    Added command sender

---
+++ Telecomando/Protocolli.IOT.Drone.Telecomando/Protocolli.IOT.Drone.Telecomando/Mqtt.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Extensions.Configuration;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Protocolli.IOT.Drone.Telecomando
+{
+    public class Mqtt
+    {
+        private readonly IMqttClient _mqttClient;
+        //private readonly MqttFactory _factory;
+        private readonly IMqttClientOptions _options;
+
+        public Mqtt()
+        {
+            // Create a new MQTT client.
+            //_factory = new MqttFactory();
+            _mqttClient = new MqttFactory().CreateMqttClient();
+
+            // Use TCP connection.
+            _options = new MqttClientOptionsBuilder()
+                .WithTcpServer("127.0.0.1",1883)
+                .Build();
+
+        }
+
+        //connect to broker
+        public void Connect()
+        {
+            //open connection with broker
+            _mqttClient.ConnectAsync(_options, CancellationToken.None);
+
+            //try to recconect when disconnected
+            _mqttClient.UseDisconnectedHandler(async e =>
+            {
+                Console.WriteLine("### DISCONNECTED FROM SERVER ###");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+
+                try
+                {
+                    await _mqttClient.ConnectAsync(_options, CancellationToken.None); // Since 3.0.5 with CancellationToken
+                }
+                catch
+                {
+                    Console.WriteLine("### RECONNECTING FAILED ###");
+                }
+            });
+        }
+
+        public async Task SendAsync (string command, int droneId)
+        {
+
+            //create message to publish
+            var message = new MqttApplicationMessageBuilder()
+            .WithTopic($"protocolliIOT/comando/drone{droneId}")
+            .WithPayload(command)
+            .WithExactlyOnceQoS()
+            .WithRetainFlag()
+            .Build();
+
+            //publish message
+            try
+            {
+                await _mqttClient.PublishAsync(message, CancellationToken.None);
+
+                Console.WriteLine($"Published Drone Status to topic: {_topic}{id}");
+            }
+            catch (MQTTnet.Exceptions.MqttCommunicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Telecomando/Protocolli.IOT.Drone.Telecomando/Protocolli.IOT.Drone.Telecomando/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Protocolli.IOT.Drone.Telecomando
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Mqtt mqtt = new Mqtt();
+            mqtt.Connect();
+            while (true){
+                await mqtt.SendAsync("Ti puzza il culo drone 0", 0);
+            }
+        }
+
+        
+
+    }
+}
