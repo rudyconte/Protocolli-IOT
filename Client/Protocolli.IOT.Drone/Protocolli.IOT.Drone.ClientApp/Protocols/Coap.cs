ad323346dd57b181e389a5e5a09b4e0f47824042
rudyconte <rudy.conte@stud.tecnicosuperiorekennedy.it>
2022-01-03 10:51:33 +0100

    Updated client to sent CoAP POST requests

---
+++ Client/Protocolli.IOT.Drone/Protocolli.IOT.Drone.ClientApp/Protocols/Coap.cs
@@ -0,0 +1,39 @@
+﻿using CoAP;
+using Protocolli.IOT.Drone.ClientApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protocolli.IOT.Drone.ClientApp.Protocols
+{
+    internal class Coap : IProtocol
+    {
+        //get the URL of the coap server
+        private readonly string _url = ConfigurationManager.AppSettings["coapServer"];
+
+        public Task SendAsync(string data)
+        {
+            //create a new pos request
+            Request _request = new(Method.POST);
+            _request.URI = new Uri(_url);
+
+            //handle reponses asynchronously with event handler
+            _request.Respond += (o, e) =>
+            {
+                Console.WriteLine("Responded with status code: " + e.Message.StatusCode);
+            };
+
+            //set the request payload
+            _request.SetPayload(data,MediaType.ApplicationJson);  
+            //send the request
+            _request.Send();
+
+            Console.WriteLine("Status sent");
+            return Task.CompletedTask;
+        }
+
+    }
+}
